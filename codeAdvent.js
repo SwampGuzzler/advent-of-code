b030fe27638019b63f44423e25e974e687c06136
Lucas Cotner <lc07@uw.edu>
2017-12-01 10:35:19 -0500

    Day One..complete!

--- codeAdvent.js
+++ codeAdvent.js
@@ -1,5 +1,14 @@
 exports.dayOne = function (digits) {
+  var exclusiveNumbers = [];
+  var total = 0;
 
+  for (var j = 0; j < digits.length; j++) {
+    if (digits.charAt(j) === digits.charAt(j + 1)) {
+      total += parseInt(digits.charAt(j));
+    } else if (j === digits.length - 1 && digits.charAt(j) === digits.charAt(0)) {
+      total += parseInt(digits.charAt(j));
+    }
+  }
 
-  return 5;
+  return total;
 }
